65623790e4288e5dfdca42471f49a326e829ee17
Satya <satyawan.singh@macmillan.com>
2017-02-23 09:27:15 -0500

    Initial Setup of the API
    Initial Setup of the API

---
+++ RestaurantSearch.API/RestaurantAPI/RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        // GET api/values/5
+        public List<Restaurant> Get(string zip)
+        {
+            //for now just return dummy restaurants.
+            return new List<Restaurant>()
+            {
+                new Restaurant() {Address="Address1",Name="RestaurantA",Zip=zip },
+                new Restaurant() {Address="Address2",Name="RestaurantA",Zip=zip },
+                new Restaurant() {Address="Address3",Name="RestaurantA",Zip=zip }
+            };
+        }
+    }
+
+}
---
+++ RestaurantSearch.API/RestaurantAPI/RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Zip { get; set; }
+        public string Address { get; set; }
+    }
+}
